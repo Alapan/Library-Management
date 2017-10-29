c786ddc02e8e04e5541f89b09e82b128e8ca7148
Alapan Mukherjee <alapan.mukherjee@medisapiens.fi>
2017-10-29 17:11:00 +0200

    Add delete book feature

--- build/js/main.js
+++ build/js/main.js
@@ -13505,6 +13505,8 @@ const Book = __WEBPACK_IMPORTED_MODULE_0_backbone___default.a.Model.extend({
     name: ''
   },
 
+  idAttribute: '_id',
+
   validate: function (attributes) {
     if (typeof attributes.firstName != 'string') {
       return 'First name is mandatory';
@@ -13624,10 +13626,36 @@ const BookView = __WEBPACK_IMPORTED_MODULE_0_backbone___default.a.View.extend({
 
   tagName: 'tr',
 
+  initialize: function () {
+    this.listenTo(this.model, 'remove', this.removeView);
+  },
+
   template: _.template(`
     <td><%= name %></td>
-    <td><%= author.first_name %> <%= author.last_name %></td>
+    <td>
+      <% if (author) { %>
+        <%= author.first_name %> <%= author.last_name %>
+      <% } %>
+    </td>
+    <td>
+      <button class="btn btn-sm btn-danger delete-btn">
+        Delete
+      </button>
+    </td>
   `),
+
+  events: {
+    'click .delete-btn': 'deleteBook'
+  },
+
+  deleteBook: function (e) {
+    e.preventDefault();
+    this.model.destroy();
+  },
+
+  removeView: function () {
+    this.remove();
+  },
 
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
